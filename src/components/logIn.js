4dd56f648352d150f5edd60c36e810cf7a25c9a2
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2020-02-24 12:51:25 +0100

    Added handleChange on LogIn component

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -2,32 +2,47 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 
-const login = (props) =>{
-
-    return(
-        <div>
-            <form>
-                <h4>Login</h4>
-                <label htmlFor={'username_of_user'}>Username:</label>
-                <div>
-                    <input type='text' name={'username_of_user'}/>
-                </div>
-                <label htmlFor={'user_password'}>Password:</label>
-                <div>
-                    <input type='text' name={'user_password'}/>
-                </div>
+class LogIn extends React.Component{
 
 
-                <div>
-                    <Link to={'/home'}>
-                        <button type='submit'>Log in</button>
-                    </Link>
-                    <button>Cancel</button>
-                </div>
-            </form>
-        </div>
-    )
+    constructor() {
+        super();
+        this.handleChange = this.handleChange.bind(this);   // mora da mu napravis bind za da mozes da koristis this vo metodot
+    }
 
-};
+    handleChange(e){                // stava vo state username i password
+        this.setState({
+            [e.target.name]:e.target.value
+        });
+    }
 
-export default login;
+    render() {
+
+        return(
+            <div>
+                <form>
+                    <h4>Login</h4>
+                    <label htmlFor={'username_of_user'}>Username:</label>
+                    <div>
+                        <input type='text' name={'username_of_user'} onChange={this.handleChange}/>
+                    </div>
+                    <label htmlFor={'user_password'}>Password:</label>
+                    <div>
+                        <input type='text' name={'password_of_user'} onChange={this.handleChange}/>
+                    </div>
+
+
+                    <div>
+                        <Link to={'/home'}>
+                            <button type='submit'>Log in</button>
+                        </Link>
+                        <button>Cancel</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default LogIn;
