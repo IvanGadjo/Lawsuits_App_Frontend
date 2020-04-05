8d8509ebb844c34cd74f007d298e1d98f9de3fa9
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2020-04-05 20:36:59 +0200

    add register new user validation

--- src/components/authentication/registerNewUser.js
+++ src/components/authentication/registerNewUser.js
@@ -1,42 +1,39 @@
-import React,{Component} from "react";
+import React, {useState} from "react";
 import {Link, withRouter} from "react-router-dom";
 import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
 
-class RegisterNewUser extends Component{
+const RegisterNewUser = (props) =>{
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedOption: "option1"
-        };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    }
+    const { register, handleSubmit, errors } = useForm(); // initialise the hook
+
+    const [selectedOption,setSelectedOption] = useState("option1");
+    const [username,setUsername] = useState("");
+    const [password,setPass] = useState("");
 
     // for radio buttons
-    handleOptionChange = (e) =>{
-        this.setState({
-            selectedOption: e.target.value
-        });
+    const handleOptionChange = (e) =>{
+        setSelectedOption(e.target.value);
     };
 
     // for username and pass
-    handleChange(e){                // stava vo state username i password
-        // console.log(e.target.name,e.target.value)
-        this.setState({
-            [e.target.name]:e.target.value
-        });
-    }
+    const handleChange = (e) =>{                // stava vo state username i password
+        if (e.target.name == "username"){
+            setUsername(e.target.value);
+        }
+        else {
+            setPass(e.target.value);
+        }
+    };
 
 
-    handleFormSubmit(e){
-        e.preventDefault();
+    const handleFormSubmit = (e) =>{
+        //e.preventDefault();
 
-        console.log(this.state)
         let role = '';
-        if (this.state.selectedOption === "option1")
+        if (selectedOption === "option1")
             role = 'supervisor';
         else
             role = 'lawyer';
@@ -48,43 +45,58 @@ class RegisterNewUser extends Component{
                 'Content-Type': 'application/json'
             },
             params:{
-                "firstName": this.state.firstName,
-                "lastName": this.state.lastName,
-                "username": this.state.username,
-                "password": this.state.password,
+                "firstName": e.firstName,
+                "lastName": e.lastName,
+                "username": username,
+                "password": password,
                 "role": role
             }
         }).then(
-            this.props.history.push('/login')
+            props.history.push('/login')
         )
-    }
+    };
 
 
-    render() {
         return(
             <div>
-                <form onSubmit={this.handleFormSubmit}>
+                <form onSubmit={handleSubmit(handleFormSubmit)}>
                     <h4>Register new user</h4>
 
                     <label htmlFor={'firstName'}>First name:</label>
                     <div>
-                        <input type='text' name={'firstName'} onChange={this.handleChange}/>
+                        <input type='text' name={'firstName'} onChange={handleChange}
+                               ref={register({
+                                   required: true
+                               })}/>
+                        {errors.firstName && <p>First name is required!</p>}
                     </div>
 
                     <label htmlFor={'lastName'}>Last name:</label>
                     <div>
-                        <input type='text' name={'lastName'} onChange={this.handleChange}/>
+                        <input type='text' name={'lastName'} onChange={handleChange}
+                               ref={register({
+                                   required: true
+                               })}/>
+                        {errors.lastName && <p>Last name is required!</p>}
                     </div>
 
 
                     <label htmlFor={'username'}>Username:</label>
                     <div>
-                        <input type='text' name={'username'} onChange={this.handleChange}/>
+                        <input type='text' name={'username'} onChange={handleChange}
+                               ref={register({
+                                   required: true
+                               })}/>
+                        {errors.username && <p>A username is required!</p>}
                     </div>
 
                     <label htmlFor={'password'}>Password:</label>
                     <div>
-                        <input type='text' name={'password'} onChange={this.handleChange}/>
+                        <input type='text' name={'password'} onChange={handleChange}
+                               ref={register({
+                                   required: true
+                               })}/>
+                        {errors.password && <p>Password is required!</p>}
                     </div>
 
 
@@ -95,8 +107,8 @@ class RegisterNewUser extends Component{
                            id={"type_supervisor"}
                            name={"type"}
                            value={"option1"}
-                           checked={this.state.selectedOption === "option1"}
-                           onChange={this.handleOptionChange}
+                           checked={selectedOption === "option1"}
+                           onChange={handleOptionChange}
                     />
                     <label htmlFor={"type_supervisor"}>Supervisor</label>
 
@@ -104,8 +116,8 @@ class RegisterNewUser extends Component{
                            id={"type_lawyer"}
                            name={"type"}
                            value={"option2"}
-                           checked={this.state.selectedOption === "option2"}
-                           onChange={this.handleOptionChange}
+                           checked={selectedOption === "option2"}
+                           onChange={handleOptionChange}
                     />
                     <label htmlFor={"type_lawyer"}>Lawyer</label>
 
@@ -123,8 +135,8 @@ class RegisterNewUser extends Component{
                 </form>
             </div>
         )
-    }
 
-}
+
+};
 
 export default withRouter(RegisterNewUser);
